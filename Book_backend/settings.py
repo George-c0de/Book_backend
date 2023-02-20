c083719b0a9e8398a0ff2ada354e755c99599159
George <82450208+George-c0de@users.noreply.github.com>
2023-02-20 14:13:59 +0300

    docker

--- Book_backend/settings.py
+++ Book_backend/settings.py
@@ -158,10 +158,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/mediafiles/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
